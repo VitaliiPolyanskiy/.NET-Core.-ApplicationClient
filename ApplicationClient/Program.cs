c9623bb514e30f2c58b39b961d8fd633c162d040
Admin <polyanskiy@itstep.academy>
2026-05-09 22:06:18 +0300

    Changing the version to .NET 10 Add comments in Ukrainian

--- ApplicationClient/Program.cs
+++ ApplicationClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Text;
 
 namespace Array
 {
@@ -9,73 +10,92 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            // Загружаем сборку
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            // Завантажуємо збірку
             Assembly asml = Assembly.LoadFrom("./SampleLibrary.dll");
-            // Выводим список типов данных, объявленный в текущем модуле
-            Console.WriteLine("Объявленные типы данных:");
+
+            // Виводимо список типів даних, оголошених у поточному модулі
+            Console.WriteLine("Оголошені типи даних:");
             foreach (Type t in asml.GetTypes())
             {
                 Console.WriteLine(t.FullName);
             }
 
-            // Получаем тип данных из сборки
+            // Отримуємо тип даних зі збірки
             Type type = asml.GetType("SampleLibrary.Employee");
 
-            // Позднее связывание позволяет создавать экземпляры некоторого типа, 
-            // а также использовать его во время выполнения приложения.
+            // Пізнє зв’язування дозволяє створювати екземпляри певного типу,
+            // а також використовувати їх під час виконання програми.
 
-            // Использование позднего связывания менее безопасно, т.к. при жестком кодировании 
-            // всех типов (ранее связывание) на этапе компиляции можно отследить многие ошибки. 
-            // В то же время позднее связывание позволяет создавать расширяемые приложения, 
-            // когда дополнительная функциональность программы неизвестна, и может быть разработана
-            // и подключена сторонними разработчиками.
+            // Використання пізнього зв’язування менш безпечне, оскільки при жорсткому кодуванні
+            // всіх типів (раннє зв’язування) на етапі компіляції можна виявити багато помилок.
+            // Водночас пізнє зв’язування дозволяє створювати розширювані застосунки,
+            // коли додаткова функціональність програми невідома та може бути розроблена
+            // і підключена сторонніми розробниками.
 
-            // Ключевую роль в позднем связывании играет класс System.Activator. 
-            // С помощью его статического метода Activator.CreateInstance() 
-            // можно создавать экземпляры заданного типа
+            // Ключову роль у пізньому зв’язуванні відіграє клас System.Activator.
+            // За допомогою його статичного методу Activator.CreateInstance()
+            // можна створювати екземпляри заданого типу.
 
-            //SampleLibrary.Employee emp = new("Иван", "Иванов", 30, "Директор", 50000M);
-            //emp.Print();
-            // Создадим объект класса Employee
+            // SampleLibrary.Employee emp = new("Іван", "Іванов", 30, "Директор", 50000M);
+            // emp.Print();
+
+            // Створимо об'єкт класу Employee
             object person = Activator.CreateInstance(type,
-                ["Иван", "Иванов", 30, "Директор", 50000M]);
+                new object[] { "Іван", "Іванов", 30, "Директор", 50000M });
+
             Console.WriteLine();
-            // Вызываем метод Print от созданного объекта
+
+            // Викликаємо метод Print у створеного об'єкта
             type.GetMethod("Print").Invoke(person, null); // person.Print();
 
             string[] files = Directory.GetFiles(".", "DLL/*.dll");
             Assembly[] asm = new Assembly[files.Length];
+
             for (int i = 0; i < files.Length; i++)
             {
                 asm[i] = Assembly.LoadFrom(files[i]);
             }
+
             List<Type> types = new List<Type>();
             for (int i = 0; i < asm.Length; i++)
             {
                 types.AddRange(asm[i].GetTypes());
             }
+
             List<MethodInfo> methodinfo = new List<MethodInfo>();
             foreach (Type t in types)
                 methodinfo.AddRange(t.GetMethods());
-            Console.WriteLine("Methods:\n\n");
+
+            Console.WriteLine("Методи:\n\n");
+
             foreach (MethodInfo info in methodinfo)
-                    Console.WriteLine(info.Name);
+                Console.WriteLine(info.Name);
 
             int[] A = new int[10];
-            methodinfo[0].Invoke(null, [A]); // public static void Init(int[] A)
-            Console.WriteLine("\nИсходный массив:\n");
-            methodinfo[1].Invoke(null, [A]); // public static void Print(int[] A)
-            methodinfo[6].Invoke(null, [A]); //  public static void Reverse(int[] A)
-            Console.WriteLine("Массив после реверсирования:\n");
-            methodinfo[1].Invoke(null, [A]); // public static void Print(int[] A)
-            methodinfo[7].Invoke(null, [A]); // public static void Neighbor(int[] A)
-            Console.WriteLine("Массив после преобразования:\n");
-            methodinfo[1].Invoke(null, [A]); // public static void Print(int[] A)
-            Console.WriteLine("Сумма элементов массива: {0,4}",
-                (int)methodinfo[12].Invoke(null, [A])); // public static int SumOfElements(int[] A)
-            Console.WriteLine("Среднее арифметическое элементов массива: {0,4}",
-                (double)methodinfo[13].Invoke(null, [A])); // public static double Average(int[] A)
-        }
 
+            methodinfo[0].Invoke(null, new object[] { A }); // public static void Init(int[] A)
+
+            Console.WriteLine("\nПочатковий масив:\n");
+            methodinfo[1].Invoke(null, new object[] { A }); // public static void Print(int[] A)
+
+            methodinfo[6].Invoke(null, new object[] { A }); // public static void Reverse(int[] A)
+
+            Console.WriteLine("Масив після реверсування:\n");
+            methodinfo[1].Invoke(null, new object[] { A }); // public static void Print(int[] A)
+
+            methodinfo[7].Invoke(null, new object[] { A }); // public static void Neighbor(int[] A)
+
+            Console.WriteLine("Масив після перетворення:\n");
+            methodinfo[1].Invoke(null, new object[] { A }); // public static void Print(int[] A)
+
+            Console.WriteLine("Сума елементів масиву: {0,4}",
+                (int)methodinfo[12].Invoke(null, new object[] { A })); // public static int SumOfElements(int[] A)
+
+            Console.WriteLine("Середнє арифметичне елементів масиву: {0,4}",
+                (double)methodinfo[13].Invoke(null, new object[] { A })); // public static double Average(int[] A)
+        }
     }
 }
